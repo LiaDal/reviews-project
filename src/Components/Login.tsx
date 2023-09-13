bf5b1cf9eada65c1ac33b2fc33c3043ff4fdb00e
LiaDal <julia18g11@gmail.com>
2023-09-13 14:56:57 +0300

    start auth

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -4,57 +4,44 @@ import Form from 'react-bootstrap/Form';
 import Navbar from './Navbar';
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  let handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      let res = await fetch("http://localhost:5173/login", {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          password: password,
-          email: email,
-        }),
-      });
-      let resJson = await res.json();
-      console.log(resJson)}
-      catch (err) {
-        console.log("err", err);
-      }
-  };
+  const handleSubmit = async (e) => {
+       e.preventDefault();
 
-
-  // const handleSubmit = e => {
-  //   e.preventDefault()
-  //   fetch('http://localhost:5173/login', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify({
-  //       email: "email",
-  //       password: "password",
-  //     })
-  // })
-  //     .then(res => res.json())
-  //     .then(data => console.log(data))
-  // }
+  await fetch('http://localhost:5173/login', {
+    method: 'POST',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({
+     name: 'name',
+     email: 'email',
+     password: 'password',
+    })
+   })
+   .then((response) => response.json())
+   .then((userData) => {
+     console.log(userData)
+   })
+   .catch((error) => {
+     console.error('err: ', error);
+   });
+  }
 
   
   return (
     <>
     <Navbar />
     <div className="d-flex justify-content-center align-item-center w-25 p-4 border shadow-sm m-5 rounded">
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Remember me" />
@@ -63,7 +50,6 @@ export default function Login() {
           <Button onSubmit={handleSubmit} variant="primary" type="submit" className="ms-5 w-50">
             Log in
           </Button>
-        <div className="message">{message ? <p>{message}</p> : null}</div>
           {/* <p className="text-center">or</p>
           <Link to="/singup">
           <Button href="singup"  className="ms-2 text-light btn-success">
--- src/Components/Singup.tsx
+++ src/Components/Singup.tsx
@@ -4,46 +4,49 @@ import Form from 'react-bootstrap/Form';
 import Navbar from './Navbar';
 
 export default function Signup() {
-  const [show, setShow] = useState(false);
 
-  const handleClose = () => { setShow(false) };
-  const handleShow = () => { setShow(true) };
-  const [data, setData] = useState(null);
-  const [query, setQuery] = useState("");
+  const [name, setName] = useState(''); 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!query) return;
+  const handleSubmit = async (e) => {
+       e.preventDefault();
 
-    async function fetchData() {
-      const response = await fetch(
-        `http://http://localhost:5173/login`
-      );
-      const data = await response.json();
-      const results = data.Search;
-      setData(results);
-      console.log(results)
-    }
-    fetchData();
-  };
+  await fetch('http://localhost:5173/singup', {
+    method: 'POST',
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({
+     name: 'name',
+     email: 'email',
+     password: 'password'
+    },)
+   })
+   .then((response) => response.json())
+   .then((userData) => {
+     console.log(userData)
+   })
+   .catch((error) => {
+     console.log('err: ', error);
+   });
+  }
 
-  
   return (
     <>
     <Navbar/>
+    <div className="d-flex justify-content-center align-item-center">
     <div className="d-flex justify-content-center align-item-center w-25 p-4 border shadow-sm m-5 rounded">
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicName">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" />
+              <Form.Control type="text" placeholder="Enter name" onChange={(e) => setName(e.target.value)} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Remember me" />
@@ -52,6 +55,7 @@ export default function Signup() {
               Sign Up
             </Button>
           </Form>
+    </div>
     </div>
    </> 
   );
