17ad721fe70ed4599fbabc7f6a338c56ef3fa804
LiaDal <julia18g11@gmail.com>
2023-09-22 08:42:07 +0300

    add logout and fix vercel

--- src/client/Components/User_page/Dashboard.tsx
+++ src/client/Components/User_page/Dashboard.tsx
@@ -2,17 +2,39 @@ import React, { useContext } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from '../Navbar'
 import Button from 'react-bootstrap/Button'
-// import { useNavigate } from 'react-router-dom'
-// import { API_URL } from '../utils/constants'
-// import { AuthContext } from '../utils/AuthContext'
+import { useNavigate } from 'react-router-dom'
+import { API_URL } from '../../utils/constants'
+import { AuthContext } from '../../utils/AuthContext'
 
 const Dashboard = () => {
+  const { authenticated, setAuthenticated } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const logout = async (e) => {
+    e.preventDefault()
+
+    await fetch(`${API_URL}/logout`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    })
+      .then(() => {
+        setAuthenticated(false)
+        navigate('/')
+      })
+      .catch((error) => {
+        console.error('err: ', error)
+      })
+  }
+
   return (
     <>
       {/* <Header /> => fetch => /me */}
       <Navbar />
       <div className="d-flex justify-content-end m-5">
-        <Button variant="dark">Log out</Button>
+        <Button onClick={logout} variant="dark">
+          Log out
+        </Button>
       </div>
     </>
   )
